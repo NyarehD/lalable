bad951772d05464aeefbc6ab60efc6a42322f3cc
Nyareh David <70676816+NyarehD@users.noreply.github.com>
2022-11-04 08:15:38 +0630

    Added: searchbox and post button

--- resources/js/Components/NavBar/NavBar.vue
+++ resources/js/Components/NavBar/NavBar.vue
@@ -1,34 +1,39 @@
 <template>
-    <header
-        class="sticky inset-0 z-10 bg-white drop-shadow-xl dark:bg-neutral-800"
-    >
+    <header class="sticky inset-0 z-10 bg-white drop-shadow-xl dark:bg-neutral-800">
         <div
-            class="customContainer flex h-16 items-center justify-between px-3 dark:bg-neutral-800 md:h-20"
-        >
+            class="flex max-w-4xl items-center justify-between px-3 dark:bg-neutral-800 h-16 mx-auto ">
             <a href="/" class="block">
                 <img
                     alt="Pokemon Logo"
                     src="/Pokemon_International.svg"
-                    class="h-14 w-full"
+                    class="h-10 w-full"
                 />
             </a>
-            <div class="flex flex-1 items-center justify-end">
-                <nav class="hidden md:block" aria-label="header-navigation">
-                    <ul class="text-co flex items-center gap-4 text-sm">
-                        <li>
-                            <NavBarLink link="/"
-                                >NewsFeed</NavBarLink
-                            >
-                        </li>
-                        <li>
-                            <NavBarLink link="dashboard">DashBoard</NavBarLink>
-                        </li>
-                        <li>
-                            <DarkModeButton />
-                        </li>
-                    </ul>
-                </nav>
-                <DarkModeButton class="mx-3 md:hidden" />
+            <div class="bg-indigo-300 rounded-full h-9 px-3 pl-5 relative">
+                <span class="absolute inset-y-0 left-0 grid place-content-center ml-3">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                         stroke="currentColor" class="w-6 h-6">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                              d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
+                    </svg>
+                </span>
+                <input class="w-full pr-10 bg-transparent focus:outline-none mt-1 ml-7">
+            </div>
+            <div class="flex items-center justify-end w-auto">
+                <div class="hidden md:block">
+                    <button class="inline-flex items-center rounded-full bg-indigo-700 text-white px-3 h-9 hover:bg-indigo-900">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                             stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                  d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125" />
+                        </svg>
+                        <span class="ml-1">Post</span>
+                    </button>
+                </div>
+                <DarkModeButton class="mx-3" />
+                <div class="">
+                    <img src="/cat.jpg" alt="" class="w-11 h-auto rounded-full">
+                </div>
                 <NavBarToggleBtn
                     @click="navBarToggle = !navBarToggle"
                     :toggle-status="navBarToggle"
@@ -58,24 +63,24 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from "vue";
-import NavBarLink from "./NavBarLink.vue";
-import NavBarToggleBtn from "./NavBarToggleBtn.vue";
-import DarkModeButton from "./DarkModeButton.vue";
+    import { ref } from "vue";
+    import NavBarLink from "./NavBarLink.vue";
+    import NavBarToggleBtn from "./NavBarToggleBtn.vue";
+    import DarkModeButton from "./DarkModeButton.vue";
 
-const navBarToggle = ref(false);
+    const navBarToggle = ref(false);
 
-function toggleNavBar() {
-    navBarToggle.value = !navBarToggle.value;
-}
+    function toggleNavBar() {
+        navBarToggle.value = !navBarToggle.value;
+    }
 </script>
 
 <style scoped lang="scss">
-.navMobile {
-    transform: translateY(-100 + 4rem);
-}
+    .navMobile {
+        transform: translateY(-100 + 4rem);
+    }
 
-.navMobile.appear {
-    transform: translateY(0);
-}
+    .navMobile.appear {
+        transform: translateY(0);
+    }
 </style>
