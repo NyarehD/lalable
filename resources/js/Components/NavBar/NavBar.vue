d48c1d4cd21daf4cd2eb446434d5522719090c07
Nyareh David <70676816+NyarehD@users.noreply.github.com>
2022-11-04 07:11:16 +0630

    Added: NavBar

---
+++ resources/js/Components/NavBar/NavBar.vue
@@ -0,0 +1,81 @@
+<template>
+    <header
+        class="sticky inset-0 z-10 bg-white drop-shadow-xl dark:bg-neutral-800"
+    >
+        <div
+            class="customContainer flex h-16 items-center justify-between px-3 dark:bg-neutral-800 md:h-20"
+        >
+            <a href="/" class="block">
+                <img
+                    alt="Pokemon Logo"
+                    src="/Pokemon_International.svg"
+                    class="h-14 w-full"
+                />
+            </a>
+            <div class="flex flex-1 items-center justify-end">
+                <nav class="hidden md:block" aria-label="header-navigation">
+                    <ul class="text-co flex items-center gap-4 text-sm">
+                        <li>
+                            <NavBarLink route-name="pokedex"
+                                >Pokedex</NavBarLink
+                            >
+                        </li>
+                        <li>
+                            <NavBarLink route-name="about">About</NavBarLink>
+                        </li>
+                        <li>
+                            <DarkModeButton />
+                        </li>
+                    </ul>
+                </nav>
+                <DarkModeButton class="mx-3 md:hidden" />
+                <NavBarToggleBtn
+                    @click="navBarToggle = !navBarToggle"
+                    :toggle-status="navBarToggle"
+                />
+            </div>
+        </div>
+        <div
+            class="navMobile absolute -z-10 mx-auto w-full max-w-screen-xl bg-white px-4 pb-4 transition-all duration-300 dark:bg-neutral-800 md:hidden md:px-8"
+            :class="{ appear: navBarToggle }"
+        >
+            <nav class="" aria-label="header-navigation">
+                <ul class="text-co flex flex-col items-center text-sm">
+                    <li class="h-10 w-full text-center align-middle">
+                        <NavBarLink route-name="pokedex" @click="toggleNavBar">
+                            Pokedex
+                        </NavBarLink>
+                    </li>
+                    <li class="h-10 w-full text-center align-middle">
+                        <NavBarLink route-name="about" @click="toggleNavBar">
+                            About
+                        </NavBarLink>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+import NavBarLink from "./NavBarLink.vue";
+import NavBarToggleBtn from "./NavBarToggleBtn.vue";
+import DarkModeButton from "./DarkModeButton.vue";
+
+const navBarToggle = ref(false);
+
+function toggleNavBar() {
+    navBarToggle.value = !navBarToggle.value;
+}
+</script>
+
+<style scoped lang="scss">
+.navMobile {
+    transform: translateY(-100 + 4rem);
+}
+
+.navMobile.appear {
+    transform: translateY(0);
+}
+</style>
---
+++ resources/js/Components/NavBar/NavBarLink.vue
@@ -0,0 +1,25 @@
+<template v-once>
+    <Link :to="props.routeName === 'pokedex' ? '/' : props.routeName">
+        <!-- Todo: change the route active -->
+        <button
+            class="relative text-lg font-medium font-medium before:absolute before:-bottom-1 before:h-0.5 before:w-full before:scale-x-0 before:bg-gray-800 before:transition hover:before:scale-x-100 dark:before:hover:bg-neutral-200"
+            :class="{
+                'text-blue-800 before:bg-blue-800 hover:text-blue-800 dark:text-cyan-400 dark:hover:text-cyan-400 dark:before:hover:bg-cyan-400': true,
+                'text-gray-500 hover:text-gray-800 dark:text-neutral-400 dark:text-neutral-400 dark:hover:text-neutral-200 dark:hover:text-neutral-200': false,
+            }"
+            @click="$emit('toggleNavBar')"
+        >
+            <slot></slot>
+        </button>
+    </Link>
+</template>
+
+<script setup>
+import { Link } from "@inertiajs/inertia-vue3";
+
+const props = defineProps({
+    routeName: String,
+});
+</script>
+
+<style scoped></style>
