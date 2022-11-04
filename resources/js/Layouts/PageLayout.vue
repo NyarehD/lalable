e32f774de9d60bbab311bad5d55e423336ab8665
Nyareh David <70676816+NyarehD@users.noreply.github.com>
2022-11-04 07:06:16 +0630

    Added: Layout for newsfeed

---
+++ resources/js/Layouts/PageLayout.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="">
+        <NavBar />
+        <h1>This is layout</h1>
+        <slot />
+    </div>
+</template>
+<script>
+import NavBar from "@/Components/NavBar/NavBar.vue";
+export default {
+    components: { NavBar },
+};
+</script>
---
+++ resources/js/Pages/NewsFeed.vue
@@ -0,0 +1,12 @@
+<template>{{ posts }}</template>
+<script>
+import PageLayout from "../Layouts/PageLayout.vue";
+export default {
+    layout: PageLayout,
+};
+</script>
+<script setup>
+defineProps({
+    posts: Object,
+});
+</script>
