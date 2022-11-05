2af118ad8ae070ce3f679e8c3c1a71d1d1f8375f
Nyareh David <70676816+NyarehD@users.noreply.github.com>
2022-11-05 20:49:44 +0630

    Fixed: margin in NewsFeed.vue

--- resources/js/Pages/Post/PostCreate.vue
+++ resources/js/Pages/Post/PostCreate.vue
@@ -1,10 +1,10 @@
 <template>
     <form @submit.prevent="submit" class="max-w-4xl mx-auto bg-gray-100 py-4 px-8 rounded-3xl"
           enctype="multipart/form-data">
-        <h1 class="text-3xl font-semibold mb-5">Create Post</h1>
+        <h1 class="text-3xl font-semibold mb-4">Create Post</h1>
         <textarea name="description" id="description" rows="4" placeholder="Write something..."
-                  class="w-full rounded-3xl p-4 focus:outline-none border-none bg-gray-200 resize-none"></textarea>
-        <div class="grid grid-cols-4 gap-2" v-if="imgList.length!==0">
+                  class="w-full rounded-3xl p-4 focus:outline-none border-none bg-gray-200 resize-none mb-4 "></textarea>
+        <div class="grid grid-cols-4 gap-2 mb-4" v-if="imgList.length!==0">
             <img :src="img" alt="img" v-for="img in imgList" class="h-[12rem] w-full object-cover rounded-3xl">
         </div>
         <div class="flex justify-between">
@@ -39,7 +39,7 @@
     function uploadAndPreviewPhoto(event) {
         for (let i = 0; i < event.target.files.length; i++) {
             imgList.value.push(URL.createObjectURL(event.target.files[i]));
-            form.photos.push(event.target.files[i])
+            form.photos.push(event.target.files[i]);
         }
     }
 
