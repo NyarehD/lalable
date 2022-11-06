6507615e5da5fd46bcfb0dedf149b9dc6bafc5fc
Nyareh David <70676816+NyarehD@users.noreply.github.com>
2022-11-06 20:28:08 +0630

    Added: Post component

---
+++ resources/js/Components/Post.vue
@@ -0,0 +1,18 @@
+<template>
+    <div
+        class="border-x-2 border-b-2 border-gray-500/50 first-of-type:border-t-2 first-of-type:rounded-t-3xl last-of-type:rounded-b-3xl">
+        <div class="grid grid-cols-2" v-if="post.post_photos.length>0">
+            <img :src="photo.full_image_path" v-for="photo in post.post_photos"
+                 class="h-[20rem] w-full object-cover rounded-b-none first-of-type:rounded-l-3xl last-of-type:rounded-r-3xl "
+                 alt=" img">
+        </div>
+        <div class="p-4" v-if="post.description">
+            <p>{{ post.description }}</p>
+        </div>
+    </div>
+</template>
+<script setup>
+    defineProps({
+        post: Object,
+    });
+</script>
--- resources/js/Pages/NewsFeed.vue
+++ resources/js/Pages/NewsFeed.vue
@@ -1,12 +1,19 @@
-<template>{{ posts }}</template>
+<template>
+    <div class="max-w-2xl mx-auto">
+        <Post v-for="post in posts" :post="post" />
+    </div>
+</template>
 <script>
-import PageLayout from "../Layouts/PageLayout.vue";
-export default {
-    layout: PageLayout,
-};
+    import PageLayout from "../Layouts/PageLayout.vue";
+
+    export default {
+        layout: PageLayout,
+    };
 </script>
 <script setup>
-defineProps({
-    posts: Object,
-});
+    import Post from "@/Components/Post.vue";
+
+    defineProps({
+        posts: Object,
+    });
 </script>
