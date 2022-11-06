88beddfc655f40b71caed2a97d9c3058e7a14e11
Nyareh David <70676816+NyarehD@users.noreply.github.com>
2022-11-06 22:03:24 +0630

    Added: Post for newsfeed

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,14 +2,26 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model {
     use HasFactory;
 
-    protected $with = ["post_photos"];
+    protected $with = ["post_photos", "owner"];
+
+    protected $appends = ["for_humans"];
+
     public function post_photos() {
         return $this->hasMany(PostPhoto::class);
+    }
+
+    public function owner() {
+        return $this->belongsTo(User::class, "user_id")->select("name", "id", "profile_picture");
+    }
+
+    protected function forHumans(): Attribute {
+        return new Attribute(get: fn() => $this->created_at->diffForHumans());
     }
 }
--- resources/js/Components/Post.vue
+++ resources/js/Components/Post.vue
@@ -1,13 +1,20 @@
 <template>
     <div
-        class="border-x-2 border-b-2 border-gray-500/50 first-of-type:border-t-2 first-of-type:rounded-t-3xl last-of-type:rounded-b-3xl">
+        class="border-x-[1px] border-b-[1px] border-gray-500/20 first-of-type:border-t-2 first-of-type:rounded-t-3xl last-of-type:rounded-b-3xl">
+        <div class="flex p-4">
+            <img :src="post.owner.full_image_path" :alt="post.owner.name" class="w-12 h-12 rounded-full">
+            <div class="ml-3">
+                <p class="font-semibold"> {{ post.owner.name }}</p>
+                <p>{{ post.for_humans }}</p>
+            </div>
+        </div>
+        <div class="px-4 pb-1" v-if="post.description">
+            <p class="text-lg">{{ post.description }}</p>
+        </div>
         <div class="grid grid-cols-2" v-if="post.post_photos.length>0">
             <img :src="photo.photo_src" v-for="photo in post.post_photos"
-                 class="h-[20rem] w-full object-cover rounded-b-none first-of-type:rounded-tl-3xl last-of-type:rounded-tr-3xl "
+                 class="h-[20rem] w-full object-cover"
                  alt=" img">
-        </div>
-        <div class="p-4" v-if="post.description">
-            <p>{{ post.description }}</p>
         </div>
     </div>
 </template>
