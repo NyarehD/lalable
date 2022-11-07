bd2c3233e2874db117fed32ddcf5524afa987f2c
Nyareh David <70676816+NyarehD@users.noreply.github.com>
2022-11-07 22:01:14 +0630

    Added: PostShow

---
+++ resources/js/Pages/Post/PostShow.vue
@@ -0,0 +1,19 @@
+<template>
+    {{ post }}
+</template>
+
+<script setup>
+    defineProps({
+        post: Object,
+    });
+</script>
+<script>
+    import PageLayout from "@/Layouts/PageLayout.vue";
+
+    export default {
+        layout: PageLayout,
+    };
+</script>
+<style scoped>
+
+</style>
