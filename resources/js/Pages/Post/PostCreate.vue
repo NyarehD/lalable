1d245e700928e30bb18bb3af2adcf43df0c0ca71
Nyareh David <70676816+NyarehD@users.noreply.github.com>
2022-11-07 21:59:28 +0630

    Added: error message in post create

--- resources/js/Pages/Post/PostCreate.vue
+++ resources/js/Pages/Post/PostCreate.vue
@@ -1,16 +1,17 @@
 <template>
-    <form @submit.prevent="submit" class="max-w-4xl mx-auto bg-gray-100 py-4 px-8 rounded-3xl"
+    <form @submit.prevent="submit" class="max-w-2xl mx-auto bg-gray-100 py-4 px-8 rounded-3xl"
           enctype="multipart/form-data">
         <h1 class="text-3xl font-semibold mb-4">Create Post</h1>
         <textarea name="description" id="description" rows="4" placeholder="Write something..."
                   class="w-full rounded-3xl p-4 focus:outline-none border-none bg-gray-200 resize-none mb-4"
                   v-model="form.description"></textarea>
+        <InputError :message="errors.description" v-if="errors.description"/>
         <PreviewPhoto :img-list="imgList" :errors="errors" />
         <div class="flex justify-between">
             <input type="file" accept="image/jpeg,image/png" class="hidden" id="photoUpload" ref="fileUpload"
                    @input="uploadAndPreviewPhoto" multiple>
             <label for="photoUpload">
-                <PrimaryBtn class="px-3">
+                <PrimaryBtn class="px-3" @click.prevent="fileUpload.click()">
                     <Media />
                 </PrimaryBtn>
             </label>
@@ -22,6 +23,7 @@
     import Media from "@/Components/Icons/Media.vue";
     import PreviewPhoto from "@/Components/PreviewPhoto.vue";
     import PrimaryBtn from "@/Components/PrimaryBtn.vue";
+    import InputError from "@/Components/InputError.vue";
     import { useForm } from "@inertiajs/inertia-vue3";
     import { ref } from "vue";
 
