db83cc9f4204e5d89c8add12b8d8d2569dd5ed93
Nyareh David <70676816+NyarehD@users.noreply.github.com>
2022-11-08 16:22:20 +0630

    Fixed: Navbar create post link not working in pages expect newsfeed

--- resources/js/Components/NavBar/NavBar.vue
+++ resources/js/Components/NavBar/NavBar.vue
@@ -23,7 +23,7 @@
                 <div class="hidden md:block mr-3">
                     <Link
                         class="inline-flex items-center primaryBtn"
-                        href="post/create">
+                        href="/post/create">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                              stroke="currentColor" class="w-6 h-6">
                             <path stroke-linecap="round" stroke-linejoin="round"
