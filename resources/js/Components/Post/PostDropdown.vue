e2879edaade2e42d2f7a835df821db5dd001923a
Nyareh David <70676816+NyarehD@users.noreply.github.com>
2022-11-09 07:24:55 +0630

    Added: Click outside to dismiss dropdown

--- resources/js/Components/Post/PostDropdown.vue
+++ resources/js/Components/Post/PostDropdown.vue
@@ -1,11 +1,11 @@
 <template>
-    <div class="relative">
+    <div class="relative" ref="dropdown">
         <PrimaryBtn class="h-10 p-2" type="text" @click="isDropdownActive=!isDropdownActive">
             <EllipsisVerticalIcon />
         </PrimaryBtn>
         <div class="absolute right-0 z-10 bg-white border-[1px] border-gray-500/20 rounded-3xl w-32"
              :class="{'fade':isDropdownActive}"
-             v-if="isDropdownActive">
+             v-show="isDropdownActive">
             <Link :href="route('post.show',{id:postId})" :headers="{foo:'bar'}"
                   class="py-2 px-6 hover:bg-gray-200/80 first-of-type:rounded-t-3xl block last-of-type:rounded-b-3xl transition-colors duration-300 cursor-pointer">
                 View
@@ -22,11 +22,16 @@
     import PrimaryBtn from "@/Components/PrimaryBtn.vue";
     import { Link } from "@inertiajs/inertia-vue3";
     import { ref } from "vue";
+    import { onClickOutside } from "@vueuse/core";
 
     defineProps({
         postId: Number,
     });
     const isDropdownActive = ref(false);
+
+    const dropdown = ref(null);
+    onClickOutside(dropdown, () => isDropdownActive.value = false);
+
 </script>
 <style>
     .fade {
