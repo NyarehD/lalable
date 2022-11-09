babefecd3d7905ee86ae446099625b772a785178
Nyareh David <70676816+NyarehD@users.noreply.github.com>
2022-11-09 07:25:44 +0630

    Added: Post show

--- resources/js/Pages/Post/PostShow.vue
+++ resources/js/Pages/Post/PostShow.vue
@@ -1,18 +1,30 @@
 <template>
-    {{ post }}
+    <!--    Do not search for solutions for overflowing in this page. That overflow is caused by laravel debug bar by adding margin-bottom: 3px to body-->
+    <div class="flex rounded-3xl relative">
+        <div class="w-3/4">
+            <PhotoCarousel :photos="post.post_photos" :is-full-height="true" v-show="true" />
+        </div>
+        <div class="border-l-[1px] border-inherit w-1/4 rounded-r-3xl">
+            <PostProfile :post="post" />
+            <div class="px-4 pb-3" v-if="post.description">
+                <p class="text-lg">{{ post.description }}</p>
+            </div>
+        </div>
+        <PrimaryBtn type="text" class="absolute top-3 left-3 p-1 text-indigo-500">
+            <XMark/>
+        </PrimaryBtn>
+    </div>
 </template>
 
 <script setup>
+    import PostProfile from "@/Components/Post/PostProfile.vue";
+    import PhotoCarousel from "@/Components/PhotoCarousel.vue";
+    import PrimaryBtn from "@/Components/PrimaryBtn.vue";
+    import XMark from "@/Components/Icons/XMark.vue";
+
     defineProps({
         post: Object,
     });
-</script>
-<script>
-    import PageLayout from "@/Layouts/PageLayout.vue";
-
-    export default {
-        layout: PageLayout,
-    };
 </script>
 <style scoped>
 
