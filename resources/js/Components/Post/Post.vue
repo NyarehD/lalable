a98e3e31e3430fe59485670c8796ddfc0eed555b
Nyareh David <70676816+NyarehD@users.noreply.github.com>
2022-11-10 17:28:49 +0630

    Added: Like model, controller and migration

--- resources/js/Components/Post/Post.vue
+++ resources/js/Components/Post/Post.vue
@@ -9,11 +9,27 @@
         <img :src="post.post_photos[0].photo_src" class="h-80 md:h-[30rem] w-full object-cover"
              v-if="post.post_photos.length===1">
         <PhotoCarousel :photos="post.post_photos" v-if="post.post_photos.length>1" />
+        <div class="flex justify-around p-1 ">
+            <PrimaryBtn type="text" class="inline-flex justify-center bg-transparent w-1/3">
+                <LikeOutline class="mr-2" />
+                Like
+            </PrimaryBtn>
+            <PrimaryBtn type="text" class="inline-flex justify-center bg-transparent w-1/3">
+                <LikeOutline class="mr-2" />
+                Like
+            </PrimaryBtn>
+            <PrimaryBtn type="text" class="inline-flex justify-center bg-transparent w-1/3">
+                <LikeOutline class="mr-2" />
+                Like
+            </PrimaryBtn>
+        </div>
     </div>
 </template>
 <script setup>
     import PhotoCarousel from "@/Components/PhotoCarousel.vue";
     import PostProfile from "@/Components/Post/PostProfile.vue";
+    import LikeOutline from "@/Components/Icons/LikeOutline.vue";
+    import PrimaryBtn from "@/Components/PrimaryBtn.vue";
     import { Inertia } from "@inertiajs/inertia";
 
     const props = defineProps({
