738f6d03da04aa570d3770006b21753ad58cc1ee
Nyareh David <70676816+NyarehD@users.noreply.github.com>
2022-12-29 23:01:02 +0630

    Added: CommentSection.vue

--- resources/js/Components/CommentSection.vue
+++ resources/js/Components/CommentSection.vue
@@ -1,8 +1,29 @@
 <template>
-    <div class="flex px-4 py-3 justify-between gap-2">
-        <img :src="user.full_image_path" alt="" class="w-11 h-auto rounded-full ">
-        <TextInput class="w-full " v-model="comment" />
-        <PrimaryBtn @click.prevent="commentPost">Comment</PrimaryBtn>
+    <div class="flex flex-col">
+        <div class="flex px-4 py-3 justify-between gap-2 grow-0">
+            <img :src="user.full_image_path" alt="" class="w-11 h-auto rounded-full ">
+            <TextInput class="w-full " v-model="comment" />
+            <PrimaryBtn @click.prevent="commentPost">Comment</PrimaryBtn>
+        </div>
+        <div class="px-4 py-3 overflow-auto" v-memo>
+            <div class="mb-4 flex" v-for="comment in comments" :key="`comments${comment.id}`">
+                <img :src="comment.user.full_image_path" :alt="comment.user.name" class="w-11 h-11 rounded-full">
+                <div class="ml-2 w-full">
+                    <div class="flex justify-between w-full items-center">
+                        <p class="font-semibold"> {{ comment.user.name }}</p>
+                        <div class="flex items-center">
+                            <p class="text-gray-900/70">{{ comment.for_humans }}</p>
+                            <DropAlt class="ml-2">
+                                <button class="dropdownButton" v-if="comment.can.is_comment_owner">
+                                    Edit
+                                </button>
+                            </DropAlt>
+                        </div>
+                    </div>
+                    <p>{{ comment.comment }}</p>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -12,6 +33,7 @@
     import TextInput from "@/Components/TextInput.vue";
     import PrimaryBtn from "@/Components/PrimaryBtn.vue";
     import { Inertia } from "@inertiajs/inertia";
+    import DropAlt from "@/Components/DropAlt.vue";
 
     const comment = ref("");
 
