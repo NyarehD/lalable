88875a605f7a2bf355d74467a1b4db0789c5d11d
Nyareh David <70676816+NyarehD@users.noreply.github.com>
2023-02-22 17:31:19 +0630

    Add: performance for news feed update

--- resources/js/Pages/NewsFeed.vue
+++ resources/js/Pages/NewsFeed.vue
@@ -1,10 +1,10 @@
 <template>
-    <div class="max-w-2xl px-2 mx-auto" v-memo>
-        <Post v-for="(post) in posts" :post="post" :key="post.id" />
+    <div class="max-w-2xl px-2 mx-auto">
+        <Post v-for="(post, i) in $props.posts" :post="post" :key="`${post.id}post${i}`" v-memo="[post.user_liked_count]" />
     </div>
-    <div v-if="!data.next_page_url">
-        No more posts
-    </div>
+    <!-- <div v-if="!data.next_page_url"> -->
+    <!-- No more posts -->
+    <!-- </div> -->
     <TextToast :text="$page.props.message" :duration="1000" />
 </template>
 <script setup lang="ts">
@@ -16,24 +16,21 @@
 
 
     const props = defineProps<{
-        data: {
-            data: any[],
-            next_page_url?: string,
-        }
+        posts: any[]
     }>();
 
-    const posts = ref(props.data.data);
+                                                // const posts = ref(props.data.data);
 
-    onMounted(() => {
-        useInfiniteScroll(window, () => {
-            if (props.data.next_page_url) {
-                fetch(props.data.next_page_url).then(response => response.json()).then(data => {
-                    posts.value = [...posts.value, ...data.data];
-                    props.data.next_page_url = data.next_page_url;
-                })
-            }
-        })
-    })
+                                                // onMounted(() => {
+                                                //     useInfiniteScroll(window, () => {
+                                                //         if (props.data.next_page_url) {
+                                                //             fetch(props.data.next_page_url).then(response => response.json()).then(data => {
+                                                //                 posts.value = [...posts.value, ...data.data];
+                                                //                 props.data.next_page_url = data.next_page_url;
+                                                //             })
+                                                //         }
+                                                //     })
+                                                // })
 </script>
 <script lang="ts">
     import PageLayout from "../Layouts/PageLayout.vue";
