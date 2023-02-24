b8cc5f79f8ae4e84f1e58f9d8e19a70b842c3ad2
Nyareh David <70676816+NyarehD@users.noreply.github.com>
2023-02-24 15:48:37 +0630

    Update: Optimization in comment section

--- resources/js/Components/Comment/CommentSection.vue
+++ resources/js/Components/Comment/CommentSection.vue
@@ -5,19 +5,19 @@
             <TextInput class="w-full " v-model="comment" @keydown.enter="commentPost" />
             <PrimaryBtn @click.prevent="commentPost" type="primary">Comment</PrimaryBtn>
         </div>
-        <div class="px-4 py-3 overflow-auto" v-memo>
-            <SingleComment v-for="comment in comments" :key="`comments${comment.id}`" :comment="comment" />
+        <div class="px-4 py-3 overflow-auto">
+            <SingleComment v-for=" comment in comments" :key="`comments${comment.id}`" :comment="comment"
+                v-memo="[comment.comment]" />
         </div>
     </div>
 </template>
 
 <script setup>
-    import { computed, ref } from "vue";
-    import { usePage } from "@inertiajs/vue3";
-    import TextInput from "@/Components/TextInput.vue";
-    import PrimaryBtn from "@/Components/PrimaryBtn.vue";
-    import { router } from "@inertiajs/vue3";
     import SingleComment from "@/Components/Comment/SingleComment.vue";
+    import PrimaryBtn from "@/Components/PrimaryBtn.vue";
+    import TextInput from "@/Components/TextInput.vue";
+    import { router, usePage } from "@inertiajs/vue3";
+    import { computed, ref } from "vue";
 
     const comment = ref("");
 
@@ -26,7 +26,6 @@
         postId: Number,
     });
     const user = computed(() => usePage().props.auth.user);
-
     function commentPost() {
         router.post(route("comment.store"), {
             "comment": comment.value,
