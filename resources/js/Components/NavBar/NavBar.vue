6edbf450a343b946d4c526d306476d236254a2d2
Nyareh David <70676816+NyarehD@users.noreply.github.com>
2023-03-03 14:53:52 +0630

    Update: navbar for small devices

--- resources/js/Components/NavBar/NavBar.vue
+++ resources/js/Components/NavBar/NavBar.vue
@@ -4,7 +4,7 @@
             <Link href="/" class="block">
             <img alt="Pokemon Logo" src="/Pokemon_International.svg" class="w-full h-10" />
             </Link>
-            <div class="relative px-3 pl-5 bg-indigo-300 rounded-full h-9">
+            <div class="relative hidden px-3 pl-5 bg-indigo-300 rounded-full h-9 md:block">
                 <span class="absolute inset-y-0 left-0 grid ml-3 place-content-center">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                         stroke="currentColor" class="w-6 h-6">
@@ -15,17 +15,17 @@
                 <input class="w-full pr-10 mt-1 bg-transparent focus:outline-none ml-7">
             </div>
             <div class="flex items-center justify-end w-auto">
-                <div class="hidden mr-3 md:block">
-                    <Link class="inline-flex items-center primaryBtn" href="/post/create">
+                <div class="mr-3">
+                    <Link class="inline-flex items-center px-3 primaryBtn" href="/post/create" v-if="user" as="button">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                         stroke="currentColor" class="w-6 h-6">
                         <path stroke-linecap="round" stroke-linejoin="round"
                             d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125" />
                     </svg>
-                    <span class="ml-1">Post</span>
+                    <span class="hidden ml-1 md:block">Post</span>
                     </Link>
                 </div>
-                <DarkModeButton class="mr-3" />
+                <DarkModeButton class="hidden mr-3 md:block" />
                 <div class="mr-3" v-if="user">
                     <Link :href="route('user.show', { user: user.id })">
                     <img :src="user.full_image_path" :alt="user.name" class="rounded-full h-11 w-11">
