c53387d40712cf9d0dfbc4d92509ee63c5e2d4e8
Nyareh David <70676816+NyarehD@users.noreply.github.com>
2023-03-03 14:53:31 +0630

    Remove: commented lines in NewsFeed

--- resources/js/Pages/NewsFeed.vue
+++ resources/js/Pages/NewsFeed.vue
@@ -2,32 +2,15 @@
     <div class="max-w-2xl px-2 mx-auto">
         <Post v-for="(post, i) in $props.posts" :post="post" :key="`${post.id}post${i}`" v-memo="[post.user_liked_count]" />
     </div>
-    <!-- <div v-if="!data.next_page_url"> -->
-    <!-- No more posts -->
-    <!-- </div> -->
     <TextToast :text="$page.props.message" :duration="1000" />
 </template>
 <script setup lang="ts">
     import Post from "@/Components/Post/Post.vue";
     import TextToast from "@/Components/TextToast.vue";
 
-
     const props = defineProps<{
         posts: any[]
     }>();
-
-                                                                            // const posts = ref(props.data.data);
-
-                                                                            // onMounted(() => {
-                                                                            //     useInfiniteScroll(window, () => {
-                                                                            //         if (props.data.next_page_url) {
-                                                                            //             fetch(props.data.next_page_url).then(response => response.json()).then(data => {
-                                                                            //                 posts.value = [...posts.value, ...data.data];
-                                                                            //                 props.data.next_page_url = data.next_page_url;
-                                                                            //             })
-                                                                            //         }
-                                                                            //     })
-                                                                            // })
 </script>
 <script lang="ts">
     import PageLayout from "../Layouts/PageLayout.vue";
