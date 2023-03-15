ab7c3b8caab7b26a8583231e139bc784a298619f
Nyareh David <70676816+NyarehD@users.noreply.github.com>
2023-03-15 20:37:52 +0630

    Add: PostCreate dark mode

--- resources/js/Pages/Post/PostCreate.vue
+++ resources/js/Pages/Post/PostCreate.vue
@@ -1,23 +1,25 @@
 <template>
-    <form @submit.prevent="submit" class="max-w-2xl px-8 py-4 mx-auto bg-gray-100 rounded-3xl"
-        enctype="multipart/form-data">
-        <h1 class="mb-4 text-3xl font-semibold">Create Post</h1>
-        <textarea name="description" id="description" rows="4" placeholder="Write something..."
-            class="w-full p-4 mb-4 bg-gray-200 border-none resize-none rounded-3xl focus:outline-none"
-            v-model="form.description"></textarea>
-        <PreviewPhoto :img-list="imgList" :errors="errors" />
-        <div class="flex justify-between">
-            <input type="file" accept="image/jpeg,image/png" class="hidden" id="photoUpload" ref="fileUploadRef"
-                @input="uploadAndPreviewPhoto" multiple>
-            <label for="photoUpload">
-                <PrimaryBtn type="primary" class="px-3" @click.prevent="fileUploadRef.click()">
-                    <Media />
-                </PrimaryBtn>
-            </label>
-            <PrimaryBtn type="primary">Create Post</PrimaryBtn>
-        </div>
-        <InputError :message="errors.description" v-if="errors.description" class="mt-3" />
-    </form>
+    <div class="h-screen">
+        <form @submit.prevent="submit" class="rounded-3xl dark:bg-neutral-800 max-w-2xl px-8 py-4 mx-auto bg-gray-100"
+            enctype="multipart/form-data">
+            <h1 class="dark:text-white mb-4 text-3xl font-semibold">Create Post</h1>
+            <textarea name="description" id="description" rows="4" placeholder="Write something..."
+                class="rounded-3xl dark:caret-white dark:text-white placeholder:text-white/70 focus:outline-none dark:bg-neutral-700 w-full p-4 mb-4 bg-gray-200 border-none resize-none"
+                v-model="form.description"></textarea>
+            <PreviewPhoto :img-list="imgList" :errors="errors" />
+            <div class="flex justify-between">
+                <input type="file" accept="image/jpeg,image/png" class="hidden" id="photoUpload" ref="fileUploadRef"
+                    @input="uploadAndPreviewPhoto" multiple>
+                <label for="photoUpload">
+                    <PrimaryBtn type="primary" class="px-3" @click.prevent="fileUploadRef.click()">
+                        <Media />
+                    </PrimaryBtn>
+                </label>
+                <PrimaryBtn type="primary">Create Post</PrimaryBtn>
+            </div>
+            <InputError :message="errors.description" v-if="errors.description" class="mt-3" />
+        </form>
+    </div>
 </template>
 <script setup lang="ts">
     import Media from "@/Components/Icons/Media.vue";
