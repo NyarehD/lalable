7712186a1cdc2480015cceff27d5307c75ab8a8c
Nyareh David <70676816+NyarehD@users.noreply.github.com>
2023-03-18 16:48:58 +0630

    Add: UserEdit Darkmode

--- resources/js/Pages/User/UserEdit.vue
+++ resources/js/Pages/User/UserEdit.vue
@@ -1,59 +1,63 @@
 <template>
-  <div class="flex flex-col max-w-4xl gap-4 mx-auto md:flex-row">
+  <div class="md:flex-row flex flex-col max-w-4xl min-h-screen gap-4 mx-auto">
     <!-- Container for Profile -->
-    <div class="static flex md:sticky top-20 md:flex-col md:h-full md:w-1/3">
+    <div class="md:sticky top-20 md:flex-col md:h-full md:w-1/3 static flex">
       <div class="flex md:flex-col rounded-3xl border-gray-500/20 border-[1px] md:mb-3">
-        <div class="relative w-1/3 md:w-full">
+        <div class="md:w-full relative w-1/3">
           <img :src="previewProfilePhoto || user.full_image_path"
-            class="object-cover w-full h-full aspect-square rounded-3xl" :alt="`profile picture of ${user.name}`" />
+            class="aspect-square rounded-3xl object-cover w-full h-full" :alt="`profile picture of ${user.name}`" />
           <button class="absolute bottom-0 left-0 px-3 md:px-4 md:py-2.5 primaryBtn" @click="profilePictureInput.click">
-            <PencilSquareIcon class="h-4 md:h-6" />
+            <PencilSquareIcon class="md:h-6 h-4" />
           </button>
           <input type="file" accept="image/jpeg,image/png" class="hidden" id="profilePicture" ref="profilePictureInput"
             @input="handleProfileInput">
           <div class="absolute bottom-0 right-0">
-            <button class="mr-1 primaryBtn" v-show="showSaveProfilePhoto" @click="submitProfilePicture">Save</button>
-            <button class="bg-red-600 primaryBtn hover:bg-red-700" v-show="showSaveProfilePhoto"
+            <button class="primaryBtn mr-1" v-show="showSaveProfilePhoto" @click="submitProfilePicture">Save</button>
+            <button class="primaryBtn hover:bg-red-700 bg-red-600" v-show="showSaveProfilePhoto"
               @click="removePreviewPhoto">Cancel</button>
           </div>
         </div>
-        <div class="flex flex-col w-2/3 p-3 md:h-full md:p-4">
+        <div class="md:h-full md:p-4 flex flex-col w-2/3 p-3">
           <div>
-            <h2 class="text-2xl">{{ user.name }}</h2>
-            <p class="mt-2">{{ user.bio ?? "Please add your bio" }}</p>
+            <h2 class="dark:text-white text-2xl">{{ user.name }}</h2>
+            <p class="dark:text-white mt-2">{{ user.bio ?? "Please add your bio" }}</p>
           </div>
         </div>
       </div>
     </div>
     <!-- Section for Profile's posts -->
-    <div class="flex flex-col px-3 rounded-3xl md:w-2/3">
+    <div class="rounded-3xl md:w-2/3 flex flex-col px-3">
       <div class="flex items-center w-full mb-3">
         <div class="w-1/4">
-          <label class="font-semibold" for="name">Name</label>
+          <label class="dark:text-white font-semibold" for="name">Name</label>
         </div>
         <div class="w-3/4">
-          <TextInput id="name" class="w-full" v-model="form.name" />
+          <TextInput id="name"
+            class="dark:bg-neutral-700 dark:text-white dark:placeholder:text-white/70 dark:caret-white w-full"
+            v-model="form.name" />
         </div>
       </div>
       <div class="flex items-center w-full mb-3">
         <div class="w-1/4">
-          <label class="font-semibold" for="email">Email</label>
+          <label class="dark:text-white font-semibold" for="email">Email</label>
         </div>
         <div class="w-3/4">
-          <TextInput id="email" class="w-full" v-model="form.email" />
+          <TextInput id="email"
+            class="dark:bg-neutral-700 dark:text-white dark:placeholder:text-white/70 dark:caret-white w-full"
+            v-model="form.email" />
         </div>
       </div>
       <div class="flex items-start w-full">
         <div class="w-1/4 mt-3">
-          <label class="font-semibold" for="bio">Bio</label>
+          <label class="dark:text-white font-semibold" for="bio">Bio</label>
         </div>
         <div class="w-3/4">
           <textarea name="bio" id="bio" rows="4" placeholder="Write something..."
-            class="w-full p-4 mb-4 bg-gray-200 border-none resize-none rounded-3xl focus:outline-none"
+            class="rounded-3xl dark:bg-neutral-700 dark:text-white dark:placeholder:text-white/70 dark:caret-white focus:outline-none w-full p-4 mb-4 bg-gray-200 border-none resize-none"
             v-model="form.bio"></textarea>
         </div>
       </div>
-      <button class="w-full primaryBtn" @click="handleForm">
+      <button class="primaryBtn w-full" @click="handleForm">
         Save
       </button>
     </div>
