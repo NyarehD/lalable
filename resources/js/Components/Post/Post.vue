3c7f22e7514b71b647146068885f842312ba67c1
Nyareh David <70676816+NyarehD@users.noreply.github.com>
2023-03-23 17:02:46 +0630

    Fix event bubbling while clicking a shared post. Fixes #13

--- resources/js/Components/Post/Post.vue
+++ resources/js/Components/Post/Post.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="bg-white dark:bg-neutral-800 dark:border-neutral-200/20 border-x-[1px] border-b-[1px] border-gray-500/20 first-of-type:border-t-2 first-of-type:rounded-t-3xl last-of-type:rounded-b-3xl hover:cursor-pointer hover:bg-gray-200/50 transition-colors duration-300 dark:text-white dark:hover:bg-neutral-700/60"
-        @click.stop="goToPost">
+        @click.stop="goToPost(post.id)">
         <PostProfile :post="post" />
         <div class="px-4 pb-3" v-if="post.description">
             <p class="text-lg">{{ post.description }}</p>
@@ -16,7 +16,8 @@
                 v-if="post.original_post.post_photos.length === 1">
             <PhotoCarousel :photos="post.original_post.post_photos" v-if="post.original_post.post_photos.length > 1" />
             <div class="bg-white dark:bg-neutral-800 dark:hover:bg-neutral-700/70 dark:border-neutral-200/20 hover:bg-white/10 mx-3 border-x-[1px] border-b-[1px] rounded-b-3xl"
-                :class="{ 'border-t-[1px] rounded-t-3xl': post.original_post.post_photos.length === 0 }">
+                :class="{ 'border-t-[1px] rounded-t-3xl': post.original_post.post_photos.length === 0 }"
+                @click.stop="goToPost(post.original_post_id)">
                 <PostProfile :post="post.original_post" />
                 <div class="px-4 pb-3" v-if="post.original_post.description">
                     <p class="text-lg">{{ post.original_post.description }}</p>
@@ -34,10 +35,10 @@
     import Post from "@/Types/post.type";
     import { router } from "@inertiajs/vue3";
 
-    const props = defineProps<{
+    defineProps<{
         post: Post
     }>()
-    function goToPost() {
-        router.visit(route("post.show", { id: props.post.id }), { preserveScroll: true });
+    function goToPost(id: number) {
+        router.visit(route("post.show", { id }), { preserveScroll: true })
     }
 </script>
